a3fd0849f8e3e34caf0482381aa65e9b88db108f
Joselin Grez <joselingrez.m@hotmail.es>
2017-07-07 14:11:49 -0300

    intentando con modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,22 +39,25 @@
     });
 });*/
 
-$(document).ready(function(){
+$(document).ready(function(){// obtengo
     $.ajax({
-        url: 'http://pokeapi.co/api/v2/pokemon',
-        type: 'GET',
-        dataType: 'json',
-        data: {"limit": '811'},
+        url: "http://pokeapi.co/api/v2/pokemon",
+        type: "GET",
+        dataType: "json",
+        data: {"limit": "811"},
     })
     .done(function(res) {
         console.log("success");
         for (var i = 1; i < 710; i++) {
-        var pokePic = $("<img src=http://pokeapi.co/media/img/" + i + ".png id=" + i + ">");
-            $('div.pokemon').append(pokePic);
+        var pokePic = $("<div class= 'col s3 m3 l3 xl3'> <img src=http://pokeapi.co/media/img/" + i + ".png id=" + i + "></div>");
+            $("div.pokemon").append(pokePic);
         }
 
-        $('img', this).click(function() {
-             event.preventDefault();
+    $("img").click(function() {
+        var miModal = $(".caja-modal");   
+        event.preventDefault();
+        miModal.append($(this)); 
+
         var uniqueId = $(this).attr('id');
         var webAddress = "http://pokeapi.co/api/v2/pokemon" + uniqueId;
         var types = " ";
@@ -64,7 +67,7 @@ $(document).ready(function(){
                 types += "<li>" + res.types[j].name + "</li>";
 
             }
-        }, 'json');
+        }, "json");
 
         $.get(webAddress, function(res) {
 
@@ -83,10 +86,6 @@ $(document).ready(function(){
 
         }, 'json');
     })
-         $(document).ready(function(){
-            // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
-            $('.modal').modal();
-        });
-    }); 
+   }); 
 
  })
